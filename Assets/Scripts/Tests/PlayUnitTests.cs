f46973c298633d5628b1ec4508dd86b26ae9fec7
Pierre Lagoutte <lagoutte_pierre@orange.fr>
2025-01-23 10:35:31 +0100

    Unit tests, connection Unity cloud

--- Assets/Scripts/Tests/PlayUnitTests.cs
+++ Assets/Scripts/Tests/PlayUnitTests.cs
@@ -27,4 +27,56 @@ public class PlayUnitTests
 
         Assert.IsTrue(GameManager.s_this.IsPlaying);
     }
+
+    [UnityTest]
+    public IEnumerator isCharacterJumping()
+    {
+        SceneManager.LoadScene(0);
+        yield return null;
+
+        List<Button> listButtons = Button.FindObjectsByType<Button>(FindObjectsSortMode.None).ToList<Button>();
+        Button startButton = listButtons.Find(x => x.name == "StartButton");
+
+        startButton.onClick.Invoke();
+        yield return null;
+
+        Button flyButton = listButtons.Find(x => x.name == "FlyUpButton");
+        flyButton.onClick.Invoke();
+        yield return null;
+
+        PlayerController player = GameObject.FindFirstObjectByType<PlayerController>();
+        Assert.IsTrue(player.GetComponent<Rigidbody>().linearVelocity.magnitude > 1);
+    }
+
+    [UnityTest]
+    public IEnumerator isLeaderboardDisplaying()
+    {
+        SceneManager.LoadScene(0);
+        yield return null;
+
+        List<Button> listButtons = Button.FindObjectsByType<Button>(FindObjectsSortMode.None).ToList<Button>();
+        Button startButton = listButtons.Find(x => x.name == "StartButton");
+
+        startButton.onClick.Invoke();
+        yield return null;
+
+        yield return new WaitUntil(() => !GameManager.s_this.IsPlaying);
+        yield return new WaitForSeconds(1.2f);
+
+        GameObject leaderboardPanel = GameObject.Find("EndScorePanel");
+        Assert.IsTrue(leaderboardPanel.activeSelf);
+    }
+
+    [UnityTest]
+    public IEnumerator isReadingFile()
+    {
+        SceneManager.LoadScene(0);
+        yield return null;
+
+        LeaderboardManager leaderboardManager = GameObject.FindFirstObjectByType<LeaderboardManager>();
+        Score[] scoresFromFile = leaderboardManager.ReadLeaderboard();
+
+        yield return null;
+        Assert.IsTrue(scoresFromFile.Length > 0);
+    }
 }
